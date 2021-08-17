cd6aca520eb41acb42531f98fdbdc6f03f3855ab
Benjo Kho <benjokho@gmail.com>
2021-08-18 01:13:37 +0800

    Add book update logic

---
+++ javascript/main.js
@@ -0,0 +1,156 @@
+const library = document.getElementById('library');
+const addBookButton = document.getElementById('new-book');
+const readButtons = document.getElementsByClassName('read-book')
+const removeButtons = document.getElementsByClassName('remove-book')
+const overlay = document.getElementById('overlay');
+const formSubmitButton = document.getElementById('submit');
+
+let books = []
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.index = books.length;
+    // this.info = function() {
+    //     let a = title + this.author;
+    //     a += read ? " finished reading" : "not yet read";
+    //     return a;
+    // }
+}
+Book.prototype.info = function() {
+    let info = "title: " + title + ", author: " + author + ", " + pages + " pages read, ";
+    info += read ? "finished reading" : "not yet read";
+    return info;
+}   
+
+addBookButton.addEventListener('click', function() {
+    overlay.style.display = "flex";
+});
+
+formSubmitButton.addEventListener('click', function() {
+    const form = new FormData(document.getElementById('book-prompt'));
+    if (form.get('prompt-title') !== "" && form.get('prompt-author') !== "" && form.get('prompt-pages') !== "") {
+        console.log(form.get('prompt-title'));
+        console.log(form.get('prompt-author'));
+        console.log(form.get('prompt-pages'));
+        generateBook();
+    }
+});
+
+function generateBook() {
+    const form = new FormData(document.getElementById('book-prompt'));
+    console.log(form);
+    // for (let pair of form.entries()) {
+    //     console.log(pair);
+    // }
+
+    let hasRead = form.has('prompt-read');
+
+    const newBook = new Book(form.get('prompt-title'), form.get('prompt-author'), form.get('prompt-pages'), hasRead);
+    addBookToLibrary(newBook);
+    // console.log(form.get('prompt-title'));
+    // console.log(form.get('prompt-author'));
+    // console.log(form.get('prompt-pages'));
+}
+
+function addBookToLibrary(bk) {
+    books.push(bk);
+
+    const book = document.createElement('article');
+    book.classList.add('book');
+    
+    const bookTitle = document.createElement('h2');
+    bookTitle.classList.add('book-title');
+    bookTitle.innerHTML = bk.title;
+    book.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement('h3');
+    bookAuthor.classList.add('book-author');
+    bookAuthor.innerHTML = bk.author;
+    book.appendChild(bookAuthor);
+
+    const bookPages = document.createElement('h3');
+    bookPages.classList.add('book-pages');
+    bookPages.innerHTML = bk.pages;
+    book.appendChild(bookPages);
+
+    const btnRead = document.createElement("button");
+    btnRead.classList.add("read-book");
+    if (!bk.read) {
+        btnRead.classList.add('toggled'); //toggled means not read!
+        btnRead.innerHTML = "Not Read";
+    }
+    else {
+        btnRead.innerHTML = "Read";
+    }
+    book.appendChild(btnRead);
+
+    const btnRemove = document.createElement("button");
+    btnRemove.classList.add("remove-book");
+    btnRemove.classList.add("bold");
+    btnRemove.innerHTML = "Remove";
+    book.appendChild(btnRemove);
+
+    library.appendChild(book);
+
+    btnRead.addEventListener('click', function() {
+        btnRead.classList.toggle('toggled');
+        if (btnRead.classList.contains('toggled')) {
+            btnRead.innerHTML = "Not Read";
+        }
+        else {
+            btnRead.innerHTML = "Read";
+        }
+        console.info("toggled!");
+    });
+    
+    
+    btnRemove.addEventListener('click', function() {
+        removeBookFromLibrary(btnRemove);
+    });
+
+    document.getElementById('book-prompt').reset();
+    overlay.style.display = "none";
+
+    // <article class="book">
+        // <h2 class="book-title">Invisible Man</h2>
+        // <h3 class="book-author">HG Wells</h3>
+        // <h3 class="book-pages">69 Pages</h3>
+        // <button class="read-book">Read</button>
+        // <button class="remove-book bold">Remove</button>
+    // </article>
+
+}
+
+function removeBookFromLibrary(book) {
+    books.pop(book.index);
+    book.parentNode.remove();
+}
+
+
+//start of MAIN
+document.getElementById('book-prompt').reset();
+
+for (let i=0; i < readButtons.length; i++) {
+    let button = readButtons[i];
+    button.addEventListener('click', function() {
+        button.classList.toggle('toggled');
+        if (button.classList.contains('toggled')) {
+            button.innerHTML = "Not Read";
+        }
+        else {
+            button.innerHTML = "Read";
+        }
+        console.info("toggled!");
+    });
+}
+
+
+for (let i=0; i < removeButtons.length; i++) {
+    let button = removeButtons[i];
+    button.addEventListener('click', function() {
+        removeBookFromLibrary(button);
+    });
+}
